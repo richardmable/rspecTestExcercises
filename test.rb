50af1ea0eb4e0be38e1bc007776651eaaeba04c2
Richard Mable <richardbmable@gmail.com>
2016-01-25 18:08:14 -0500

    added more tests for Article class

--- test.rb
+++ test.rb
@@ -7,7 +7,10 @@ describe Article do
 	let(:q_author){Article.new(title: "This is the q one", body: "Blah blah blah blah blah", author: "Quacker")}
 	let(:new_article_no_title){Article.new(body: "Blah blah blah blah blah", author: "Richard")}
 	let(:new_article_no_body){Article.new(title: "There is no body", author: "Richard")}
+	let(:new_article_no_author){Article.new(title: "There is no body", body: "Blah blah blah blah blah")}
+	let(:new_article_with_new_lines){Article.new(title: "War and Peace", body: "Blah blah blah blah blah \n new line \n new line \n new line \n new line", author: "Richard Mable")}
 
+	#falsey == treat nil as false
 	#check title?
 	it '.title? should return nil if empty' do
 		#expect the article instance with no title, running the title method, to return false
@@ -45,13 +48,41 @@ describe Article do
 	end
 
 	it '.title_as_html should return string with h1 tags' do
-		expect(new_article_all_args.title_as_html).to eq("<h1>#{@title}<h1>")
+		expect(new_article_all_args.title_as_html).to eq("<h1>#{new_article_all_args.title}</h1>")
 	end
-	
 
-# def title_as_html
-# "<h1>#{title}</h1>"
+	it '.title_as_html should return just h1 tags if title is nil' do
+		expect(new_article_no_title.title_as_html).to eq("<h1>#{new_article_no_title.title}</h1>")
+	end
+
+	it '.author_as_html should return string with b tags' do
+		expect(new_article_all_args.author_as_html).to eq("<b>#{new_article_all_args.author}</b>")
+	end
+
+	it '.author_as_html should return just b tags if author is nil' do
+		expect(new_article_no_author.author_as_html).to eq("<b>#{new_article_no_author.author}</b>")
+	end
+
+	it '.body_as_html should return string with p tags' do
+		expect(new_article_all_args.body_as_html).to eq(new_article_all_args.body.split("\n").map{|paragraph| "<p>#{paragraph}</p>"}.join(""))
+	end
+
+	it '.body_as_html should return string with multiple p tags' do
+		expect(new_article_with_new_lines.body_as_html).to eq(new_article_with_new_lines.body.split("\n").map{|paragraph| "<p>#{paragraph}</p>"}.join(""))
+	end
+
+	it '.body_as_html should return NoMethodError' do
+		expect(new_article_no_body.body_as_html).to eq(NoMethodError)
+	end
+
+
+
+
+#  def body_as_html
+#   body.split("\n").map{|paragraph| "<p>#{paragraph}</p>"}.join("")
 # end
+
+
 
 end
 
